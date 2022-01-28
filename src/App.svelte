ceb34e89e54497c27fee49423733c4feba772c24
Anand raja <46064269+actionanand@users.noreply.github.com>
2022-01-29 01:08:31 +0530

    fetch api to get data

--- src/App.svelte
+++ src/App.svelte
@@ -3,23 +3,43 @@
 
   let hobbyInput;
   let hobbies = [];
+  let isLoading = false;
+
+  fetch('https://vue-http-exmp-default-rtdb.firebaseio.com/svelte-hobbies.json')
+    .then(res => {
+      if(!res.ok) {
+        throw new Error('Error fatching the hobbies')
+      }
+      return res.json();
+    })
+    .then(data => {
+      hobbies = Object.values(data);
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
 
   function addHobby() {
     hobbies = [hobbyInput.value, ...hobbies];
-    hobbyInput.value = '';
+
+    isLoading = true;
 
     fetch('https://vue-http-exmp-default-rtdb.firebaseio.com/svelte-hobbies.json', {
       method: 'POST',
-      body: JSON.stringify(hobbies),
+      body: JSON.stringify(hobbyInput.value),
       headers: {
         'Content-Type': 'application/json'
       }
     }).then(res => {
+      isLoading = false;
+      hobbyInput.value = '';
       if(!res.ok) {
         throw new Error('Error saving the hobby')
       }
       console.log(res);
     }).catch(err => {
+      isLoading = false;
+      hobbyInput.value = '';
       console.log(err.message);
     });
   }
@@ -36,11 +56,15 @@
 </div>
 
 <div>
-  <ul>
-    {#each hobbies as hobby}
-      <li>{hobby}</li>
-    {/each}
-  </ul>
+  {#if isLoading}
+    <p>Loading...</p>
+  {:else}
+    <ul>
+      {#each hobbies as hobby}
+        <li>{hobby}</li>
+      {/each}
+    </ul>
+  {/if}
 </div>
 
 <style>
